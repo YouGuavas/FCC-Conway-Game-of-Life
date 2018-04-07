1a1f25209b053d79b3cb23b4e3962c2645d8d3e0
YouGuavas <patrickyambrick@gmail.com>
2018-04-06 23:39:32 -0400

    Add comments, add speed adjustment buttons

--- src/Game.js
+++ src/Game.js
@@ -11,8 +11,8 @@ export class Game extends Component {
 			let max = this.state.width * this.state.height;
 			let indices = [td+1, td-1, td+this.state.height, td-this.state.height, td+this.state.height+1, td+this.state.height-1, td-this.state.height+1, td-this.state.height-1]
 			let count = 0;
-			//if (a[td].status === 'young') console.log(td, indices);
 			indices.map(element => {
+				//Count living neighbor cells
 				if (element >= 0 && element < max) {
 					if (a[element].status === 'old' || a[element].status === 'young') { count++ };
 				} 
@@ -30,23 +30,26 @@ export class Game extends Component {
 		for (let c = 0; c < this.state.cells.length; c++) {
 			const counter = getCount(c);
 			if (counter === 3 && a[c].status === 'dead') {
+				//If a dead cell has exactly 3 neighbors, bring it to life
 				cells.push({id:c, status:'young'});
 			}
 			if ((counter === 2 || counter === 3) && (a[c].status === 'young' || a[c].status === 'old')) {
+				//If a living cell has 2 or 3 neighbors, advance its age
 				cells.push({id:c, status:'old'});
 			}
 			if (counter === 2 && a[c].status === 'dead') {
+				//If a status is dead, stay dead. This seems redundant, but I did some silliness that makes this necessary.
 				cells.push({id:c, status:'dead'})
 			}
 			if ((counter < 2 || counter > 3)) {
+				//If too many or too few living neighbors, die
 				cells.push({id:c, status:'dead'});
 			}
 		}
-		//console.log(a);
-		//console.log(cells);
 		let check = 0;
 		for (let i = 0; i < cells.length; i++) {
 			if (cells[i].status === 'dead') {check++}
+				//Check if whole board has died
 		};
 		if (check !== a.length) {
 			this.setState({
@@ -68,6 +71,7 @@ export class Game extends Component {
 		}
 		for (let i = 0; i < cells.length/3; i++) {
 			cells[Math.floor(Math.random()*cells.length)].status = 'young';
+			//Randomly populate 33% of the board with young living cells
 		}
 		this.setState({
 			cells,
@@ -79,13 +83,16 @@ export class Game extends Component {
 	}
 	componentDidMount() {
 		this.start();
+		//Automatically start the game
 	}
 	handleCellClick(e) {
 		const myself = document.getElementById(e.target.id);
 		myself.className === 'dead' ? myself.className = 'young' : myself.className = 'dead';
+		//If cell is dead, bring it to life, and vice versa
 		let cells = Array.prototype.map.call(document.getElementsByTagName('td'), element => {
 			return {id:element.id, status: element.className}
 		});
+		//Update state to reflect new cell status
 		this.setState({
 			cells
 		});
@@ -119,6 +126,7 @@ export class Game extends Component {
 			this.gameOfLife();
 			if(this.state.running === 1) {
 				this.timer();
+				//If we're still running, repeat the process
 			}
 		}, this.state.speed);
 	}
@@ -126,6 +134,16 @@ export class Game extends Component {
 		this.setState({
 			running:1
 		}, () => {this.timer();});
+	}
+	setTime(d) {
+		this.setState({
+			running:0,
+			speed:d
+		}, () => {
+			this.setState({
+				running:1
+			})
+		})
 	}
 	constructor(props) {
 		super(props);
@@ -135,7 +153,7 @@ export class Game extends Component {
 			cells: [],
 			running:0,
 			activeButton:'40x20',
-			speed:100,
+			speed:300,
 			generation: 0
 		}
 		//Game Functions
@@ -159,11 +177,15 @@ export class Game extends Component {
 						<Button val="50x30" className={this.state.activeButton === '50x30' ? 'active' : ''} handleClick={() => {this.setDimensions(50,30);}}/>
 						<Button val="70x50" className={this.state.activeButton === '70x50' ? 'active' : ''} handleClick={() => {this.setDimensions(70,50);}}/>
 					</section>
-					<Button val="Run" handleClick={this.start}/>
-					<Button val="Pause" handleClick={this.pause}/>
+					<Button val="Run" handleClick={this.start} className={this.state.running === 1 ? 'active' : ''}/>
+					<Button val="Pause" handleClick={this.pause} className={this.state.running === 0 ? 'active' : ''}/>
 					<Button val="Clear" handleClick={this.clear} />
 				</section>
 				<Cells height={this.state.height} width={this.state.height} cells={this.state.cells} handleClick={this.handleCellClick}/>
+				<section>
+				 <Button val="Slow" className={this.state.speed=== 300 ? 'active' : ''} handleClick={() => {this.setTime(300)}}/>
+				 <Button val="Fast" className={this.state.speed=== 5 ? 'active' : ''} handleClick={() => {this.setTime(5)}}/>
+				</section>
 			</section>
 			)
 	}
